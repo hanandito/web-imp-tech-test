85f98fad8b3f0cdfff4ac13ad9a9446aaf16c4f8
teddypicker <hananditos@gmail.com>
2021-03-04 14:28:46 +0700

    initial commit

---
+++ CommonNumbers.php
@@ -0,0 +1,20 @@
+<?php
+$B = [5408, 6604, 32158, 84984, 8774, 34871];
+ $nilai = null;
+ $sukuken = 0;
+ $i = 0;
+ foreach($B as $valueB){
+	$i++;
+	if (isNull($nilai1)){
+		$nilai = $valueB;
+		$sukuken = $i;
+	}else{
+		if ($nilai > $valueB){
+			$nilai = $valueB;
+		    $sukuken = $i;
+		}
+	}
+ }
+ 
+ echo "suku ke-" . $sukuken . " dengan nilai = " . $nilai;
+ ?>
