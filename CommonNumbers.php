761d4caf2f4d10a447dcaf7f3390f9f0b8c69ee5
teddypicker <hananditos@gmail.com>
2021-03-05 10:35:17 +0700

    initial commit

--- CommonNumbers.php
+++ CommonNumbers.php
@@ -3,10 +3,10 @@ $A =  [68168, 87954, 32158, 8774];
 $B = [5408, 6604, 32158, 84984, 8774, 34871];
 
 $result = [];
-foreach($A as $valueA){
-   foreach($B as $valueB){
-	   if ($valueA == $valueB){
-		   array_push($result, $valueA);
+foreach($A as $nilaiA){
+   foreach($B as $nilaiB){
+	   if ($nilaiA == $nilaiB){
+		   array_push($result, $nilaiA);
 		   break;
 	   }
    }
